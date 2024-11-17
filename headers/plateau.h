7002f5c134b2657c5017031ab6e34fc5a0e71a6d
Nathan L <nathan.ledesma@edu.ece.fr>
2024-11-17 13:52:58 +0100

    Finition du code pour initialiser les joueurs (pseudo, nombre de joueurs, etat)

--- headers/plateau.h
+++ headers/plateau.h
@@ -1,15 +1,1 @@
-//
-// Created by nathan on 01/11/2024.
-//
 
-#ifndef PLATEAU_H
-#define PLATEAU_H
-
-#define TAILLE 9
-int plateau[TAILLE][TAILLE];
-void deplacement_joueur();
-//Déplace le pion du joueur suivant sont choix
-void placement_barriere();
-//Place une barrière sur le plateau comme indiqué par le joueur
-
-#endif //PLATEAU_H
