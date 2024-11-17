15cbab97526388c9b216a3d97db440966508dd5e
Nathan L <nathan.ledesma@edu.ece.fr>
2024-11-17 22:24:32 +0100

    Fonctions de menu.c définies dans menu.h

--- headers/menu.h
+++ headers/menu.h
@@ -1,6 +1,11 @@
 #ifndef MENU_H
 #define MENU_H
+#include "joueur.h"
 
-
+int menu();
+int tirage_au_sort(int nb_joueurs);
+int rotation_joueur(int aleatoire);
+void deplacer_pion(int tour_joueur, Players joueur[]);
+int menu_action(int tour_joueur, Players joueur[]);
 
 #endif //MENU_H
