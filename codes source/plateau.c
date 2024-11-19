e54cb38d851a5a21269ef7408c221096a3aef488
Nathan L <nathan.ledesma@edu.ece.fr>
2024-11-19 23:52:08 +0100

    Correction dans chargement_plateau : oublie d'un & pour le fscanf.

--- codes source/plateau.c
+++ codes source/plateau.c
@@ -58,7 +58,7 @@ void chargement_plateau(Players joueur[], int* nbjoueurs) {
     //Lecture des informations et assignation des données à chaques joueurs
     for (int i = 0; i < *nbjoueurs; i++) {
         fscanf(fplateau, "%s %d %c %d %d %d %d %d \n",
-        joueur[i].nom, &joueur[i].etat, &joueur[i].pion, &joueur[i].barriere, &joueur[i].coord_x, &joueur[i].coord_y, &joueur[i].score, joueur[i].couleur);
+        joueur[i].nom, &joueur[i].etat, &joueur[i].pion, &joueur[i].barriere, &joueur[i].coord_x, &joueur[i].coord_y, &joueur[i].score, &joueur[i].couleur);
     }
         fclose(fplateau);
 }
