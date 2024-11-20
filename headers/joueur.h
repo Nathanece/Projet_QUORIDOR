2a5448be881e32f6c1177ba9240f2d729534e2f3
Philippe <Philippe.nicolas@edu.ece.fr>
2024-11-20 12:31:38 +0100

    j'ai ajouter un s

--- headers/joueur.h
+++ headers/joueur.h
@@ -10,7 +10,7 @@ typedef struct {
     int coord_x, coord_y;
     int score;
     int couleur;
-} Players; //Structure des joueurs
+} Players; //Structures des joueurs
 
 
 void initialisation_joueurs(Players joueur[4], int *nb_joueurs);
