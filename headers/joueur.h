04242a3c52c05917a24e54375369089d0a798997
Nathan L <nathan.ledesma@edu.ece.fr>
2024-11-28 15:39:22 +0100

    Ajout de commentaires dans les headers pour les fonctions. Déjà présent pour les .c, permet de comprendre rapidement le role des fonctions avec les headers

--- headers/joueur.h
+++ headers/joueur.h
@@ -13,11 +13,17 @@ typedef struct {
     int coord_victoire;
 } Players; //Structures des joueurs
 
+//Fonction d'enregistrement des infos des joueurs pour une nouvelle partie
 void creation_joueurs(Players joueur[4], int *nb_joueurs);
+//Reinitialisation des informations (coordonnées et barrieres) au debut de chaques nouvelles manches
 void reinitialisation_coord_joueurs(Players joueur[], int nb_joueurs);
+//Sauvegarde du score après une manche terminée
 void sauvegarder_scores(Players joueur[], int nb_joueurs);
+//Chargement des scores lors de la reprise d'une ancienne partie
 void charger_scores(Players joueur[], int nb_joueurs);
+//Augmentation du score lors de la victoire d'un joueur
 void mettre_a_jour_scores(Players joueur[], int gagnant);
+//Verification de la fin de la partie (lorsqu'un joueur à atteint le bord opposé)
 void verification_fin(Players joueur[], int nb_joueur, int* fin);
 
 
--- headers/menu.h
+++ headers/menu.h
@@ -3,15 +3,20 @@
 #include "joueur.h"
 #include "plateau.h"
 
+//Menus affichés aux joueurs (soit avant une partie, soit pour choisir son action)
 void menu(int *choix);
-int tirage_au_sort(int nb_joueurs);
-void deplacer_pion(int tour_joueur, Players joueur[], int nb_joueurs, int compteur_barrieres, Barriere_plateau barrieres[]);
 int menu_action(int tour_joueur, Players joueur[]);
-void fiche_aide();
-void poser_barriere(Barriere_plateau barrieres[20], Players joueur[], int *compteur_barriere, int tour_joueur);
+//Tirage au sort pour savoir quel joueur commence et changement de tour pour la suite
+int tirage_au_sort(int nb_joueurs);
 void changement_tour(int* tour_joueur, int nb_joueurs);
-void afficher_scores(Players joueur[], int nb_joueurs);
+//Fiche d'aide pour savoir comment joueur
 void fiche_aide();
+//Actions réalisée à la suite d'un choix
+void deplacer_pion(int tour_joueur, Players joueur[], int nb_joueurs, int compteur_barrieres, Barriere_plateau barrieres[]);
+void poser_barriere(Barriere_plateau barrieres[20], Players joueur[], int *compteur_barriere, int tour_joueur);
+//Affichage des scores (choix dans le menu principal)
+void afficher_scores(Players joueur[], int nb_joueurs);
+//Fermer le jeu si le joueur décide d'arreter
 int quitter_jeu();
 
 #endif //MENU_H
